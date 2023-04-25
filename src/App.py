1753439501bad540fd5e8f1dd7c771006970f8d6
Domenique Hunter <domeniquehutner@gmail.com>
2023-04-25 16:48:22 +0200

    Added minimal App for testing

--- src/App.py
+++ src/App.py
@@ -3,21 +3,59 @@ import config
 import flist
 
 
+def notes():
+    print('0: back\n1: Inbox\n2: Outbox\n3: Trash\nr <nr>: read note\nq: quit')
+    while True:
+        cmd = input('> ')
+        
+        if cmd == 'q':
+            exit()
+        if cmd == '0':
+            print('\n...back..\n\n')
+            break
+        
+        if cmd == '1':
+            inbox_notes = flist.notes.get_inbox()
+            for note in inbox_notes:
+                print(note)
+            print()
+            
+        if cmd == '2':
+            outbox_notes = flist.notes.get_outbox()
+            for note in outbox_notes:
+                print(note)
+            print()
+        
+        if cmd.startswith('r'):
+            try:
+                _, nr = cmd.split(' ')
+                print(flist.notes.get_note_text(nr))
+                print()
+            except:
+                pass
+
 def main():
-    request = flist.login.login(config.USER, config.PASSWORD)
-    # print(request.content)
     
+    # login
+    request = flist.login.login(config.USER, config.PASSWORD)
+    
+    # get characters    
     flist.characters.find_characters(request.content.decode('utf-8'))
     
-    notes = flist.notes.get_inbox()
-    print(notes[0])
-    print(notes[0].show())
-    
-    status = flist.notes.send_note(dest, source, title, text)
-    print(status, status.text)
-    
-    status = flist.notes.delete_note([notes[0].note_id])
-    print(status, status.text)
+    print('h: help\nq: quit')
+    while True:
+        cmd = input('> ')
+        
+        if cmd == 'q' or cmd == 'Q' or cmd == 'quit' or cmd == 'exit':
+            exit()
+            
+        if cmd == 'h':
+            print('q: quit\nn: notes')
+            
+        if cmd == 'n':
+            notes()
+            
+        print(cmd)
     
 if __name__ == "__main__":
     main()
